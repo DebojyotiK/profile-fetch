7ea0ecf7c4a857476b8e3174d8630ce056da2269
debojyoti <debojyoti.karmakar@neebal.com>
2023-12-03 02:21:10 +0530

    Added files

---
+++ android/app/src/main/kotlin/com/example/profile_fetch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.profile_fetch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/profile_state.dart
@@ -0,0 +1,38 @@
+enum ProfileState {
+  notVisible,
+  loading,
+  loaded,
+}
+
+class ProfileDTO {
+  final String imageUrl;
+  final String name;
+
+  ProfileDTO({
+    required this.imageUrl,
+    required this.name,
+  });
+}
+
+class ProfileInfo {
+  final ProfileState _state;
+
+  ProfileState get state => _state;
+  final ProfileDTO? _profileDTO;
+
+  ProfileDTO get profileDTO => _profileDTO!;
+
+  ProfileInfo._(this._state, this._profileDTO);
+
+  ProfileInfo.notVisible()
+      : _state = ProfileState.notVisible,
+        _profileDTO = null;
+
+  ProfileInfo.loading()
+      : _state = ProfileState.loading,
+        _profileDTO = null;
+
+  ProfileInfo.loaded(ProfileDTO profileDTO)
+      : _state = ProfileState.loading,
+        _profileDTO = profileDTO;
+}
